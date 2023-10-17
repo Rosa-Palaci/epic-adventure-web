642372f57bde596e7be4cafb699655591378bea8
Rosa-Palaci <palacibelrose431@gmail.com>
2023-10-16 22:40:09 -0600

    created screen student-login

---
+++ epic-adventure-web/src/screens/student-login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function StudentLogin() {
+  return (
+    <div>
+      <h2>Contenido de la página de Estudiante</h2>
+    </div>
+  );
+}
+
+export default StudentLogin;
